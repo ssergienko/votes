7cfaffa92f987741ab984eca715bde81c85ee4ed
Sergei Sergienko <sergey.s.sergienko@gmail.com>
2022-12-05 22:30:34 +0100

    show questions and users online

---
+++ src/app/room/constants.ts
@@ -0,0 +1,3 @@
+export const constants = {
+  USER_ID_FORMAT_LENGTH: 16
+};
--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { APIService, RoomUser, RoomQuestion, Room } from '../API.service';
+import { APIService, RoomUser, RoomQuestion, Room, ListRoomQuestionsQuery, ListRoomUsersQuery } from '../API.service';
 import { ActivatedRoute } from "@angular/router";
 import { Subscription } from 'rxjs';
+import { constants } from './constants';
 
 @Component({
   selector: 'app-room',
@@ -12,15 +13,16 @@ export class RoomComponent implements OnInit {
 
   private routeSub: Subscription;
 
-  public questions: RoomQuestion[] = [];
-  public users: RoomUser[] = [];
+  public questions: ListRoomQuestionsQuery;
+  public users: ListRoomUsersQuery;
   private interval: ReturnType<typeof setInterval>;
   private room: Room;
 
   constructor(private api: APIService, private route: ActivatedRoute) {
     this.routeSub = this.route.params.subscribe(params => {
       localStorage.setItem('room_id', params['id']);
-      this.createUser();
+      this.initializeUser();
+      this.getRoomQuestions();
     });
   }
 
@@ -28,7 +30,7 @@ export class RoomComponent implements OnInit {
     this.interval = setInterval(() => this.getOnlineUsers(), 1000);
   }
 
-  private createUser() {
+  private initializeUser() {
     const userId = localStorage.getItem('user_id');
     if(!userId) {
       this.api.CreateUser({
@@ -53,26 +55,24 @@ export class RoomComponent implements OnInit {
           this.api.CreateRoomUser({userId: localStorage.getItem('user_id'), roomId: localStorage.getItem('room_id')}).then((roomUser) => {
             console.log('User added to the room: ', roomUser);
         });
-        this.getRoomQuestions();
-        this.getOnlineUsers();
       }
     });
   }
 
-  private getOnlineUsers():void {
+  private getOnlineUsers() {
     this.api.ListRoomUsers({
       roomId: { contains: localStorage.getItem('room_id') }
       }).then((roomUsers) => {
+        this.users = roomUsers;
         console.log('Users online: ', roomUsers);
       });
   }
 
-  private getRoomQuestions():void {
+  private getRoomQuestions() {
     this.api.ListRoomQuestions({
-      roomId: {
-        contains: localStorage.getItem('room_id')
+        roomId: {contains: localStorage.getItem('room_id')
       }}).then((questions) => {
-        // this.questions = questions.items;
+        this.questions = questions;
         console.log('Room questions: ', questions);
       });
   }
@@ -84,12 +84,13 @@ export class RoomComponent implements OnInit {
 
   // generates random string as a unique room id
   private generateId():string {
-    return Math.random().toString(16).slice(2);
+    return Math.random().toString(constants.USER_ID_FORMAT_LENGTH).slice(2);
   }
 
   private generateName():string {
     let names = ['Apple', 'Banana', 'Mango', 'Orange', 'Watermelon', 'Pear']; // array
     let index = Math.floor(Math.random() * names.length);
+    console.log('username: ', names[index]);
     return names[index];
   }
 
